44f845092e8b14626623e32339b06a2e77bd675f
mouctare <mouctard78@gmail.com>
2021-06-14 15:54:33 +0200

    Mise en place de la route de sendMessage dans le serveur

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,33 +4,37 @@ import Formulaire from './Formulaire'
 import axios from "axios";
 
 
-export default class Contact extends Component {
+class Contact extends Component {
+
     componentDidMount = () =>{
         document.title = "Page de Contact"
     }
 
-    handleEnvoiMail = (message) =>{
-        axios.post("http://localhost/SERVEURANIMAUX/client/sendMessage", message)
-             .then(response => {
-                 console.log(response);
-             })
-             .catch(error => {
-                console.log(error);
-             })
+    handleEnvoiMail = (message) => {
+      axios.post("http://localhost/SERVEURANIMAUX/client/sendMessage", message)
+          .then(reponse=> {
+              console.log(reponse)
+          })
+          .catch(error => {
+              console.log(error)
+          })
+  }
 
-    }
   render() {
-    return (
-      <> <TitreH1 bgColor="bg-success">Contactez nous !</TitreH1> 
-      <div className="container">
-          <h2>Adresse :</h2>
-          xxxxxxxxxxx
-          <h2>Téléphone</h2>
-          00 00 00 00
-          <h2>Vous préférez nous écrire ?</h2>
-        <Formulaire sendMail = {this.handleEnvoiMail}/>
-      </div>
-      </>
-    );
+      return (
+          <>
+              <TitreH1 bgColor="bg-success">Contactez nous !</TitreH1>
+              <div className="container">
+                  <h2>Adresse :</h2>
+                  9, Rue de la paix
+                  <h2>Téléphone</h2>
+                  04-05-33-66
+                  <h2>Vous préfèrez nous écrire ? </h2>
+                  <Formulaire sendMail = {this.handleEnvoiMail}/>
+              </div>
+          </>
+      );
   }
 }
+
+export default Contact;
